20c4715c26284f682dd878ab99a396a07aabb762
eze stom <ezequielstom@gmail.com>
2023-07-15 18:02:25 -0300

    finished Js en la web y manipulacion del DOM class

--- Manipulando-dom-con-js-proyecto_inicial/script.js
+++ Manipulando-dom-con-js-proyecto_inicial/script.js
@@ -1,24 +1,14 @@
 const btn = document.querySelector("[data-form-btn]");
 const input = document.querySelector("[data-form-input]");
-const task = document.createElement("li");
 const list = document.querySelector("[data-list]");
 
 function createTask(e) {
 	e.preventDefault();
 	const value = input.value;
 	input.value = "";
-	const content = `
-            <i class="far fa-check-square icon"></i>
-            <span class="task">${value}</span>
-          
-          <i class="fas fa-trash-alt trashIcon icon"></i>`;
-	task.innerHTML = content;
-	const taskContent = document.createElement("div");
-	taskContent.appendChild(checkComplete());
-	task.classList.add("card");
-	list.appendChild(task);
 
 	if (value === "") {
+		// El input está vacío
 		input.classList.add("error");
 		Swal.fire({
 			position: "top",
@@ -28,13 +18,62 @@ function createTask(e) {
 			timer: 1500,
 		});
 	} else {
+		// El input no está vacío
 		input.classList.remove("error");
+
+		const task = document.createElement("li");
+		task.classList.add("card");
+
+		const taskContent = document.createElement("div");
+		taskContent.classList.add("task");
+		taskContent.innerText = value;
+
+		const trashIcon = document.createElement("i");
+		trashIcon.classList.add("fas", "fa-trash-alt", "trashIcon", "icon");
+
+		const checkIcon = document.createElement("i");
+		checkIcon.classList.add("far", "fa-check-square", "icon");
+		let isChecked = false; // Variable de estado
+
+		checkIcon.addEventListener("click", (e) => {
+			isChecked = !isChecked; // Alternar el estado al hacer clic
+			if (isChecked) {
+				checkIcon.classList.add("checked");
+				taskContent.classList.add("completed"); // Aplicar estilo completado
+				// Agrega aquí la lógica adicional cuando la tarea esté completa
+			} else {
+				checkIcon.classList.remove("checked");
+				taskContent.classList.remove("completed"); // Eliminar estilo completado
+				// Agrega aquí la lógica adicional cuando la tarea ya no esté completa
+			}
+		});
+		trashIcon.addEventListener("click", (e) => {
+			Swal.fire({
+				title: "Atención",
+				text: "Deseas borrar tu tarea?",
+				icon: "warning",
+				showCancelButton: true,
+				confirmButtonColor: "#3085d6",
+				cancelButtonColor: "#d33",
+				confirmButtonText: "Si, borrar!",
+			}).then((result) => {
+				if (result.isConfirmed) {
+					Swal.fire(
+						"Borrada!",
+						"Tu tarea no se encuentra más en la lista.",
+						"success"
+					);
+					list.removeChild(task);
+				}
+			});
+		});
+
+		task.appendChild(trashIcon);
+		task.appendChild(taskContent);
+		task.appendChild(checkIcon);
+
+		list.appendChild(task);
 	}
 }
 
 btn.addEventListener("click", createTask);
-const checkComplete = () => {
-	const i = document.createElement("i");
-	i.classList.add("far", "fa-check-square", "icon");
-	return i;
-};
