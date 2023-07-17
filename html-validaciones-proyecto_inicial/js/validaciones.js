e7e77894dc07425c04822093ff67735147b4814c
eze stom <ezequielstom@gmail.com>
2023-07-17 14:46:59 -0300

    end proyect validate html form

--- html-validaciones-proyecto_inicial/js/validaciones.js
+++ html-validaciones-proyecto_inicial/js/validaciones.js
@@ -1,38 +1,79 @@
-const inputNacimiento = document.querySelector("#birth");
+export function valida(input) {
+	const tipoDeInput = input.dataset.tipo;
+	if (validadores[tipoDeInput]) {
+		validadores[tipoDeInput](input);
+	}
 
-function inputsErrores() {
-	const inputs = document.querySelectorAll("input");
-	inputs.forEach((input) => {
-		input.addEventListener("blur", (e) => {
-			if (!input.checkValidity()) {
-				input.parentElement.classList.add("input-container--invalid");
-			} else {
-				input.parentElement.classList.remove(
-					"input-container--invalid"
-				);
-			}
-		});
-	});
+	if (input.validity.valid) {
+		input.parentElement.classList.remove("input-container--invalid");
+		input.parentElement.querySelector(".input-message-error").innerHTML =
+			"";
+	} else {
+		input.parentElement.classList.add("input-container--invalid");
+		input.parentElement.querySelector(".input-message-error").innerHTML =
+			mostrarMensajeDeError(tipoDeInput, input);
+	}
 }
 
-inputNacimiento.addEventListener("blur", (e) => {
-	validarNacimiento(e.target);
-});
+const tipoDeErrores = [
+	"valueMissing",
+	"typeMismatch",
+	"patternMismatch",
+	"customError",
+];
+
+const mensajesDeError = {
+	nombre: {
+		valueMissing: "El campo nombre no puede estar vacío",
+	},
+	email: {
+		valueMissing: "El campo correo no puede estar vacío",
+		typeMismatch: "El correo no es válido",
+	},
+	password: {
+		valueMissing: "El campo contraseña no puede estar vacío",
+		patternMismatch:
+			"Al menos 6 caracteres, máximo 12, debe contener una letra minúscula, una letra mayúscula, un número y no puede contener caracteres especiales.",
+	},
+	nacimiento: {
+		valueMissing: "Este campo no puede estar vacío",
+		customError: "Debes tener al menos 18 años de edad",
+	},
+};
+
+const validadores = {
+	nacimiento: (input) => validarNacimiento(input),
+};
+
+function mostrarMensajeDeError(tipoDeInput, input) {
+	let mensaje = "";
+	tipoDeErrores.forEach((error) => {
+		if (input.validity[error]) {
+			console.log(tipoDeInput, error);
+			console.log(input.validity[error]);
+			console.log(mensajesDeError[tipoDeInput][error]);
+			mensaje = mensajesDeError[tipoDeInput][error];
+		}
+	});
+	return mensaje;
+}
+
 function validarNacimiento(input) {
 	const fechaCliente = new Date(input.value);
 	let mensaje = "";
-	if (!mayorEdad(fechaCliente)) {
-		mensaje = "Debes ser mayor de edad para registrarte";
+	if (!mayorDeEdad(fechaCliente)) {
+		mensaje = "Debes tener al menos 18 años de edad";
 	}
+
 	input.setCustomValidity(mensaje);
 }
 
-function mayorEdad(fecha) {
+function mayorDeEdad(fecha) {
 	const fechaActual = new Date();
 	const diferenciaFechas = new Date(
 		fecha.getUTCFullYear() + 18,
 		fecha.getUTCMonth(),
 		fecha.getUTCDate()
 	);
-	return fechaActual >= diferenciaFechas;
+	return diferenciaFechas <= fechaActual;
 }
