cd46187220cc662a8e6759dca14d58010b1c6764
eze stom <ezequielstom@gmail.com>
2023-08-13 17:27:04 -0300

    finish crud_js_async

--- CRUD_JS_Async-proyecto_base/controllers/actualizar-controller.js
+++ CRUD_JS_Async-proyecto_base/controllers/actualizar-controller.js
@@ -1,22 +1,33 @@
 import { api } from "../service/api.js";
+const redireccionamiento = "http://127.0.0.1:5500/CRUD_JS_Async-proyecto_base";
+
+const nombre = document.querySelector("[data-nombre]");
+const email = document.querySelector("[data-email]");
+const form = document.querySelector("#formulario");
 
 const obtenerInformacion = async () => {
-	const url = new URLSearchParams(window.location.search);
+	const url = new URL(window.location);
+	const id = url.searchParams.get("id");
 
-	const id = url.get("id");
-	api.detalleCliente(id)
-		.then((res) => {
-			const nombre = document.querySelector("[data-nombre]");
-			const email = document.querySelector("[data-email]");
-			nombre.value = res.nombre;
-			email.value = res.email;
-         console.log(res);
-		})
-		.catch((error) => {
-			console.error(error);
+	if (id === null) {
+		window.location.href = "/screens/error.html";
+	}
+	try {
+		await api.detalleCliente(id).then((response) => {
+			nombre.value = response.nombre;
+			email.value = response.email;
 		});
-
-
+	} catch (error) {
+		console.error(error);
+	}
 };
 
 obtenerInformacion();
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+	const url = new URL(window.location);
+	const id = url.searchParams.get("id");
+	api.actualizarCliente(nombre.value, email.value, id);
+	window.location.href = `${redireccionamiento}/screens/registro_completado.html`;
+});
--- CRUD_JS_Async-proyecto_base/service/api.js
+++ CRUD_JS_Async-proyecto_base/service/api.js
@@ -22,9 +22,26 @@ const eliminarCliente = (id) => {
 
 const detalleCliente = (id) => fetch(`${URL}/${id}`).then((res) => res.json());
 
+const actualizarCliente = async (nombre, email, id) => {
+	try {
+		const res = await fetch(`${URL}/${id}`, {
+			method: "PUT",
+			headers: {
+				"Content-type": "application/json",
+			},
+			body: JSON.stringify({ nombre, email, id }),
+		});
+		const res_1 = await res.json();
+		return console.log(res_1);
+	} catch (error) {
+		return console.error(error);
+	}
+};
+
 export const api = {
 	clientesList,
 	createCliente,
 	eliminarCliente,
 	detalleCliente,
+	actualizarCliente,
 };
